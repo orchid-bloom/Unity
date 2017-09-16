bb9fd1cceb06b5c78a79270f5be3b4c8827bbcbe
TemaSir <tianxin@gsoft.cc>
2017-09-16 14:37:36 +0800

    update

---
+++ 1-UGUIProject/Assets/GameManger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManger : MonoBehaviour {
+
+	public void OnStartGame(string sceneName) {
+		Application.LoadLevel (sceneName);
+	}
+
+//	public void OnStartGame(int index) {
+//		Application.LoadLevel (index);
+//	}
+}
